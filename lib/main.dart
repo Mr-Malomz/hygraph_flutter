73b1d7ac0a9c0ffab1b3f57da02da9a96c906c6f
demola malomo <malomo.alaba@yahoo.com>
2024-03-09 23:07:28 +0100

    integrate hygraph

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hygraph_flutter/screens/home.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final HttpLink httpLink = HttpLink("URL GOES HERE");
+
+final ValueNotifier<GraphQLClient> client =
+    ValueNotifier<GraphQLClient>(GraphQLClient(
+  link: httpLink,
+  cache: GraphQLCache(),
+));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'News App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'News App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Home(),
       ),
-      home: const Home(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hygraph_flutter/components/news_row.dart';
+import 'package:hygraph_flutter/utils.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,10 +10,28 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+String getNewsQuery = """
+query MyQuery {
+  trendings {
+    newsCollection {
+      ... on NewsRow {
+        id
+        preview
+        title
+        coverImage {
+          createdAt
+          fileName
+          url
+        }
+      }
+    }
+  }
+}
+""";
+
 class _HomeState extends State<Home> {
   @override
   void initState() {
-    //todo
     super.initState();
   }
 
@@ -24,34 +43,59 @@ class _HomeState extends State<Home> {
             const Text('Trending News', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
       ),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {},
-            child: Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(width: .5, color: Colors.grey),
-                ),
-              ),
-              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      body: Query(
+        options: QueryOptions(document: gql(getNewsQuery)),
+        builder: (result, {fetchMore, refetch}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: Colors.black,
+            ));
+          }
+
+          List<News>? newsList =
+              (result.data?["trendings"][0]["newsCollection"] as List<dynamic>)
+                  .map((item) => News.fromJson(item))
+                  .toList();
+
+          if (newsList.isEmpty) {
+            return const Text('No news yet!');
+          }
+
+          return ListView.builder(
+            itemCount: newsList.length,
+            itemBuilder: (context, index) {
+              final news = newsList[index];
+              return InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(width: .5, color: Colors.grey),
+                    ),
+                  ),
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                  child: Row(
                     children: [
-                      NewsRow(
-                          url:
-                              "https://images.unsplash.com/photo-1701505708183-59f709e7d4e6?q=80&w=3264&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                          date: "2024-03-08",
-                          preview: "Sample news preview $index",
-                          title: "Sample news title $index")
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          NewsRow(
+                            url: news.coverImage.url,
+                            preview: news.preview,
+                            title: news.title,
+                          )
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           );
         },
       ),
